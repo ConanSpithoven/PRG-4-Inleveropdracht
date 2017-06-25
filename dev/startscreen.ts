51175a0447173d4c81dc7d06fd12040058296f20
Conan <Conan.spithoven@gmail.com>
2017-06-25 14:41:10 +0200

    Update startscreen.ts

--- dev/startscreen.ts
+++ dev/startscreen.ts
@@ -5,10 +5,6 @@ class StartScreen{
     private button: HTMLElement;
     constructor(){
 
-        // this.background = document.createElement("div");
-        // document.body.appendChild(this.background);
-        // this.background.setAttribute("id", "overlay");
-
         this.background = document.createElement("div");
         document.body.appendChild(this.background);
         this.background.setAttribute("id", "startScreen");
@@ -18,7 +14,6 @@ class StartScreen{
         this.background.setAttribute("id", "startScreenInside");
 
         inside.innerHTML = "<h3>Hero Trainer</h3><h4>A training program to being a hero</h4>"
-
         
         this.button = document.createElement("button");
         inside.appendChild(this.button);
